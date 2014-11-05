9194f75e2e57b19e34d69d572adb47dec8ff7bb7
Eduard Carreras <ecarreras@gisce.net>
2014-11-05 10:18:10 +0100

    Update only date if greater
    This is to solve a race condition in parallelization when older reads
    are pushed after newer reads

--- empowering_api/giscedata_lectures.py
+++ empowering_api/giscedata_lectures.py
@@ -5,6 +5,16 @@ class GiscedataLecturesComptador(osv.osv):
     _name = 'giscedata.lectures.comptador'
     _inherit = 'giscedata.lectures.comptador'
 
+    def update_empowering_last_measure(self, cursor, uid, ids, last_measure):
+        upd_ids = []
+        for comp in self.read(cursor, uid, ids, ['empowering_last_measure']):
+            if last_measure > comp['empowering_last_measure']:
+                upd_ids.append(comp['id'])
+        self.write(cursor, uid, upd_ids, {
+            'empowering_last_measure': last_measure
+        })
+        return len(upd_ids)
+
     _columns = {
         'empowering_last_measure': fields.datetime('Last meassure sent')
     }
