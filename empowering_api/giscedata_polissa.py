3138ed883dc88c1f96464113d6c3fc726142709e
abbse <aleix.badia.somenergia@gmail.com>
2015-06-01 14:16:51 +0100

    Added support to new BeeData API specification Contract description:   buildingData   profile   customisedGroupingCriteria   customisedServiceParameters

--- empowering_api/giscedata_polissa.py
+++ empowering_api/giscedata_polissa.py
@@ -7,7 +7,30 @@ class GiscedataPolissa(osv.osv):
     _inherit = 'giscedata.polissa'
 
     _columns = {
+        'empowering_contractId': fields.char('Empowering contractId', size=50),
         'etag': fields.char('ETag', size=50),
     }
 
 GiscedataPolissa()
+
+
+class GiscedataPolissaModcontractual(osv.osv):
+    _name = 'giscedata.polissa.modcontractual'
+    _inherit = 'giscedata.polissa.modcontractual'
+
+    def write(self, cursor, uid, ids, vals, context=None):
+
+        res = super(GiscedataPolissaModcontractual, self).write(cursor, uid, ids, vals,
+                                                                context)
+
+        # TODO: Check whether new EmpoweringProfile must be added (renting,...) or not (power increase,...)
+        return res
+
+    def create(self, cursor, uid, vals, context=None):
+
+        # TODO: Update EmpowerinProfile update in order to link to new modcontractual.
+        res = super(GiscedataPolissa, self).create(cursor, uid, vals, context)
+        return res
+
+
+GiscedataPolissaModcontractual()
