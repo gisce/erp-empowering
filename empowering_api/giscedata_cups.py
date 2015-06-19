73e14a61d3245dc8e7563be86858878cc83d5d72
abbse <aleix.badia.somenergia@gmail.com>
2015-06-19 17:33:09 +0100

    Fixed empowering model definition in order to properly include references from modcontractual and service/profile

---
+++ empowering_api/giscedata_cups.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from osv import osv, fields
+import uuid
+
+class GiscedataCupsPs(osv.osv):
+    _name = 'giscedata.cups.ps'
+    _inherit = 'giscedata.cups.ps'
+
+    _columns = {
+        'empowering': fields.boolean('Empowering enabled', propi="no"),
+    }
+
+    _defaults = {
+        'empowering': lambda *a: False,
+    }
+
+GiscedataCupsPs()
