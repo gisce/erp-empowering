e6c63e18d8f2de8893789191cd9a37947d1aea9a
oriolpiera <oriol.piera@somenergia.coop>
2018-11-26 12:42:36 +0100

    Add field last measure for F5D and P5D

--- empowering_api/giscedata_lectures.py
+++ empowering_api/giscedata_lectures.py
@@ -4,18 +4,21 @@ class GiscedataLecturesComptador(osv.osv):
     _name = 'giscedata.lectures.comptador'
     _inherit = 'giscedata.lectures.comptador'
 
-    def update_empowering_last_measure(self, cursor, uid, ids, last_measure):
+    def update_empowering_last_measure(self, cursor, uid, ids, last_measure, type_measure='empowering_last_measure'):
+	#Default type_measure=empowering_last_measure (for back compatibility) can be empowering_last_f5d_measure and empowering_last_p5d_measure
         upd_ids = []
-        for comp in self.read(cursor, uid, ids, ['empowering_last_measure']):
-            if last_measure > comp['empowering_last_measure']:
+        for comp in self.read(cursor, uid, ids, [type_measure]):
+            if last_measure > comp[type_measure]:
                 upd_ids.append(comp['id'])
         self.write(cursor, uid, upd_ids, {
-            'empowering_last_measure': last_measure
+            type_measure: last_measure
         })
         return len(upd_ids)
 
     _columns = {
         'empowering_last_measure': fields.datetime('Last meassure sent')
+        'empowering_last_f5d_measure': fields.datetime('Last meassure sent')
+        'empowering_last_p5d_measure': fields.datetime('Last meassure sent')
     }
 
 GiscedataLecturesComptador()
