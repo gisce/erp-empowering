9febec2ead594170f6a43478613321d012da0b48
Eduard Carreras <ecarreras@gisce.net>
2020-03-27 21:59:51 +0100

    Refactor

--- empowering_api/giscedata_lectures.py
+++ empowering_api/giscedata_lectures.py
@@ -6,8 +6,7 @@ class GiscedataLecturesComptador(osv.osv):
     _name = 'giscedata.lectures.comptador'
     _inherit = 'giscedata.lectures.comptador'
 
-    def update_empowering_last_measure(self, cursor, uid, ids, last_measure, type_measure='empowering_last_measure'):
-	#Default type_measure=empowering_last_measure (for back compatibility) can be empowering_last_f5d_measure and empowering_last_p5d_measure
+    def update_empowering_measure(self, cursor, uid, ids, last_measure, type_measure):
         upd_ids = []
         for comp in self.read(cursor, uid, ids, [type_measure]):
             if last_measure > comp[type_measure]:
@@ -16,6 +15,22 @@ class GiscedataLecturesComptador(osv.osv):
             type_measure: last_measure
         })
         return len(upd_ids)
+
+    def update_empowering_last_measure(self, cursor, uid, ids, last_measure,
+                                       type_measure='empowering_last_measure'):
+        return self.update_empowering_measure(
+            cursor, uid, ids, last_measure, type_measure
+        )
+
+    def update_empowering_last_f5d_measure(self, cursor, uid, ids, last_measure):
+        return self.update_empowering_measure(
+            cursor, uid, ids, last_measure, 'empowering_last_f5d_measure'
+        )
+
+    def update_empowering_last_p5d_measure(self, cursor, uid, ids, last_measure):
+        return self.update_empowering_measure(
+            cursor, uid, ids, last_measure, 'empowering_last_p5d_measure'
+        )
 
     def get_aggregated_measures(self, cursor, uid, ids, date_start):
         sql_path = get_module_resource(
@@ -33,5 +48,6 @@ class GiscedataLecturesComptador(osv.osv):
         'empowering_last_f5d_measure': fields.datetime('Last F5D meassure sent'),
         'empowering_last_p5d_measure': fields.datetime('Last P5D meassure sent')
     }
+
 
 GiscedataLecturesComptador()
