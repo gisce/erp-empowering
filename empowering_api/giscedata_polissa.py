1c9a80e46d2c852054c39c5fec18b92b986731e5
Eduard Carreras <ecarreras@gisce.net>
2020-04-26 00:16:11 +0200

    Add empowering fields to polissa

--- empowering_api/giscedata_polissa.py
+++ empowering_api/giscedata_polissa.py
@@ -1,13 +1,35 @@
 # -*- coding: utf-8 -*-
 from osv import osv, fields
+from sql.aggregate import Max
 
 
 class GiscedataPolissa(osv.osv):
     _name = 'giscedata.polissa'
     _inherit = 'giscedata.polissa'
 
+    def _fnc_empowering_last_invoice_measure(self, cursor, uid, ids, field_name, arg, context=None):
+        if not context:
+            context = {}
+        res = dict.fromkeys(ids, False)
+        q = self.pool.get('giscedata.lectures.comptador').q(cursor, uid)
+        sql = q.select([
+            'polissa',
+            Max('empowering_last_measure'),
+        ], group_by=('polissa', )).where([
+            ('polissa', 'in', ids)
+        ])
+        cursor.execute(*sql)
+        for row in cursor.fetchall():
+            res[row[0]] = row[1]
+        return res
+
     _columns = {
         'etag': fields.char('ETag', size=50),
+        'empowering_last_invoice_measure': fields.function(
+            _fnc_empowering_last_invoice_measure, type='datetime', method=True,
+            string='Last F1 meassure sent'
+        ),
+        'empowering_last_profile_measure': fields.datetime('Last F5D meassure sent'),
         'empowering_profile': fields.many2one('empowering.modcontractual.profile', 'Empowering profile'),
         'empowering_service': fields.many2one('empowering.modcontractual.service', 'Empowering services')
     }
