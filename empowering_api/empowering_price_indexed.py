adcf171f9e7bcdbd4a5c5d50a5d9f011edca51ae
puig <francescpuig7@gmail.com>
2021-04-16 14:34:16 +0200

    Set model

--- empowering_api/empowering_price_indexed.py
+++ empowering_api/empowering_price_indexed.py
@@ -3,17 +3,16 @@ from osv import osv, fields
 
 class EmpoweringPriceIndexed(osv.osv):
     _name = 'empowering.price.indexed'
-
-    def _ff_name(self, cursor, uid, context):
-        # todo: ff to return a name of emp_price_indexed with 'TARIFF + COST'
-        pass
+    _order = 'empowering_price_indexed_last_push desc'
 
     _columns = {
-        'tarif_id': fields.char('Tariff'),
-        'cost': fields.integer('FEE'),
-        "name": fields.function(_ff_name, type="text", method=True, store=True, string="Name"),
+        'etag': fields.char('ETag', size=50),
+        'tariff_id': fields.char('Tariff', size=64, required=True),
+        'cost': fields.integer('FEE', required=True),
         'empowering_price_indexed_last_push': fields.datetime("Last indexed group data push"),
     }
+
+    _sql_constraints = [('name_uniq', 'unique (tariff_id, cost)', u'Duplicated empowering price indexed group')]
 
 EmpoweringPriceIndexed()
 
